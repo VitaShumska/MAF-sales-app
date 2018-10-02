b7235276305b050ca678530f9fd19aab1b7aee95
VitaShumska <vitaliya.shumska@gmail.com>
2018-10-02 10:46:46 +0300

    Change input params in filter component

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output, EventEmitter} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { ApiService } from '../../services/api.service';
 
 @Component({
